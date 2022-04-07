5db9f51438dd9ebd929b5a721aaeafd6d2f48fb1
Katherine Aguirre <katherine.aguirre@kinandcarta.com>
2022-04-07 15:57:58 -0300

    fix: fixed alias path not found and error render in jest

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import persistStore from 'redux-persist/es/persistStore'
 
 import App from './App'
 import store from './redux/store'
-// import reportWebVitals from './reportWebVitals'
 
 import './index.scss'
 
 const persistor = persistStore(store)
+const root = createRoot(document.getElementById('root')!)
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.unregister()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals()
