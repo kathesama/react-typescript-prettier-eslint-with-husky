1670419e749b6fa86032177044ee25f474410b39
Katherine Aguirre <katherine.aguirre@kinandcarta.com>
2022-04-07 10:54:20 -0300

    perf: added alias path config

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import cssStyle from './landTest.module.scss';
-import logo from "../../logo.svg";
 
 const Home:FC<any> = ({ props }): any => (
     <div className={cssStyle.App}>
