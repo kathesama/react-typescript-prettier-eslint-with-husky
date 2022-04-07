a349a6c3dda9d2a2d4ffe26dd90699e5598c91f2
Katherine Aguirre <katherine.aguirre@kinandcarta.com>
2022-04-07 11:05:50 -0300

    fix: added craco to fix path alias at webpack without eject

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,10 +1,14 @@
 import React, { FC } from 'react';
 import cssStyle from './landTest.module.scss';
+import logo from '@/logo.svg';
 
 const Home:FC<any> = ({ props }): any => (
+  <>
+    <img src={logo} className={cssStyle.AppLogo} alt="logo" />
     <div className={cssStyle.App}>
         Eureka!
     </div>
+  </>
   );
 
 export default Home;
